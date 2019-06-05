9df6e286f53544efa98feb553bebba3dab99e5ea
Carlos Meza <carlos@digitalr00ts.com>
2019-06-04 22:45:01 -0700

    initial commit

---
+++ src/sample/__main__.py
@@ -0,0 +1,18 @@
+""" __main_to load package as module """
+import logging
+import sys
+
+from . import cli
+
+
+def main():
+    """
+    Main entry point.
+    """
+    module_args = sys.argv[1:]
+    logging.debug("Module arguments received: %s", module_args)
+    cli.main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/sample/cli.py
@@ -0,0 +1,38 @@
+"""cm
+Sample Python Project
+
+Usage:
+  sample (-h | --help)
+  sample (-v | --version)
+
+Options:
+  -v --version                  Show version
+  -h --help                     Show this screen
+"""
+
+import logging
+
+from typing import Optional
+
+import pkg_resources
+
+from docopt import docopt
+
+
+version: Optional[str] = None
+logging.basicConfig(level=logging.DEBUG)
+_LOG = logging.getLogger(__name__)
+
+try:
+    version = pkg_resources.get_distribution(__package__).version
+except pkg_resources.DistributionNotFound:
+    version = None
+
+
+def main():
+    arguments = docopt(__doc__, version=version, options_first=False)
+    _LOG.debug("Arguments received: %s", arguments)
+
+
+if __name__ == "__main__":
+    main()
