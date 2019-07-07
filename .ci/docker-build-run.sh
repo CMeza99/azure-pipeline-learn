ce21b02d76a19f848c45c2030e6344ead426c242
Carlos Meza <carlos@digitalr00ts.com>
2019-07-06 18:12:52 -0700

    moved dockerfile run to external file

---
+++ .ci/docker-build-run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+OS="$(grep '^ID=' /etc/os-release | sed 's/ID=\(.*\)/\1/')"
+if [ "${OS}" = 'debian' ]; then
+  apt-get -qq update
+  DEBIAN_FRONTEND=noninteractive apt-get -yqq --no-install-suggests --no-install-recommends install \
+    git \
+    libc6-dev libffi-dev gcc g++ make \
+     > /dev/null
+  rm -rf -- /var/lib/apt/lists/* /var/cache/apt/*
+  useradd -ms /bin/sh -u 888 -UG users tox
+elif [ "${OS}" = 'alpine' ]; then
+  apk add --no-cache \
+    shadow \
+    git \
+    libc-dev libffi-dev gcc g++ make \
+    > /dev/null
+  useradd -mu 888 -UG users tox
+  apk del --no-cache shadow
+else
+  printf '\nERROR: Could not determine Linux disto.\n'
+  exit 1
+fi
+printf '[global]\nno-cache-dir = true\ndisable-pip-version-check = true' > /etc/pip.conf
+pip --quiet install -r /tmp/requirements.txt
